dc2d40d4d67939a4b3d5fdc72fc4db75ddeec59f
fsarachu <fsarachu@gmail.com>
2017-04-10 17:51:32 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <h1>Quotes</h1>
+        <ul>
+            <li v-for="quote in quotes">{{ quote.id }}: {{ quote.content }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'app',
+        data () {
+            return {
+                quotes: {}
+            }
+        },
+        mounted() {
+            axios.get('http://vue-laravel.dev/api/quotes')
+                .then(response => {
+                    this.quotes = response.data.quotes;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    }
+</script>
