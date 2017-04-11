ae752ef45d47e9bee3f9c825329fd4bb0528b57e
fsarachu <fsarachu@gmail.com>
2017-04-10 23:58:12 -0300

    Add Header view component

---
+++ src/views/layouts/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="nav has-shadow">
+        <div class="container">
+
+            <div class="nav-center">
+                <router-link :to="{name: 'home'}" class="nav-item is-tab" exact>Home</router-link>
+                <router-link :to="{name: 'quotes'}" class="nav-item is-tab">Quotes</router-link>
+            </div>
+
+        </div>
+    </nav>
+</template>
