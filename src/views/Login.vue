ecf3850b58209bdbb704a2165124de428df6a392
fsarachu <fsarachu@gmail.com>
2017-04-11 20:52:54 -0300

    Add login view component

---
+++ src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+    <section class="hero is-fullheight is-primary is-bold">
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns is-vcentered">
+
+                    <div class="column is-4 is-offset-4">
+
+                        <h1 class="title">Login</h1>
+
+                        <div class="box">
+
+                            <div class="field">
+                                <label class="label">Email</label>
+                                <p class="control">
+                                    <input class="input" type="email" autofocus>
+                                </p>
+                            </div>
+
+                            <div class="field">
+                                <label class="label">Password</label>
+                                <p class="control">
+                                    <input class="input" type="password">
+                                </p>
+                            </div>
+
+                            <hr>
+
+                            <div class="field">
+                                <p class="control">
+                                    <button class="button is-primary is-large is-fullwidth">Login</button>
+                                </p>
+                            </div>
+
+                        </div>
+
+                        <p class="has-text-centered">
+                            <a href="login.html">Login</a>
+                            |
+                            <a href="#">Need help?</a>
+                        </p>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </section>
+</template>
