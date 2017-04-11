56d10c5a50c51e142fd60679014d849fe459bde4
fsarachu <fsarachu@gmail.com>
2017-04-11 20:52:36 -0300

    Move header as route component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
     <div id="app">
-        <app-header></app-header>
+        <router-view name="header-top"></router-view>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
     import store from './store/store';
-    import AppHeader from './views/layouts/Header.vue';
 
     export default {
         store,
-        components: {
-            AppHeader
-        },
     };
 </script>
