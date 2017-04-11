c81228cdb614678aba54d3a80294013be0a607ba
fsarachu <fsarachu@gmail.com>
2017-04-10 23:56:59 -0300

    Add NewQuote component

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,27 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <div class="field has-addons">
+            <p class="control is-expanded">
+                <input v-model="quoteContent" class="input" type="text" placeholder="Enter an inspiring quote">
+            </p>
+            <p class="control">
+                <button class="button is-primary">Submit</button>
+            </p>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                quoteContent: ''
+            }
+        },
+        methods: {
+            onSubmit() {
+                this.$emit('quoteAdded', quoteContent);
+            }
+        }
+    }
+</script>
