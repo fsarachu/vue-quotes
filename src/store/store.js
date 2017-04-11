94a4bd6e3b6279851106478c1f2c2d963ff4225f
fsarachu <fsarachu@gmail.com>
2017-04-11 02:48:46 -0300

    Add loadQuotes mutation and action, add  addQuote action to send new quote to the backend

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
@@ -16,13 +17,25 @@ export default new Vuex.Store({
         }
     },
     mutations: {
+        loadQuotes: (state, payload) => {
+            state.quotes = payload;
+        },
         addQuote: (state, payload) => {
             state.quotes.unshift(payload);
         },
     },
     actions: {
+        loadQuotes: context => {
+            axios.get('http://vue-laravel.dev/api/quotes')
+                .then(({data}) => context.commit('loadQuotes', data.quotes))
+                .catch(error => console.log(error));
+        },
         addQuote: (context, payload) => {
-            context.commit('addQuote', payload);
+            axios.post('http://vue-laravel.dev/api/quotes', {
+                content: payload
+            })
+                .then(({data}) => context.commit('addQuote', data.quote))
+                .catch(error => console.log(error));
         },
     }
 });
