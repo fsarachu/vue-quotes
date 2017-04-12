6eb3882dbded28c63fc118a22dd52b494720052a
fsarachu <fsarachu@gmail.com>
2017-04-12 18:58:45 -0300

    Now main.js just bootstraps the application

--- src/main.js
+++ src/main.js
@@ -1,15 +1,3 @@
-import Vue from 'vue';
-import axios from 'axios';
-import App from './components/App.vue';
-import router from './router';
+import App from './bootstrap/app';
 
-// axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-Vue.prototype.$axios = axios;
-
-new Vue({
-    el: '#app',
-    router,
-    render: h => h(App)
-});
+new App();
