5c9abd29895a965374f2388b4dd1bbcddbc78124
fsarachu <fsarachu@gmail.com>
2017-04-13 00:38:40 -0300

    Route transition

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,7 +1,10 @@
 <template>
     <div id="app">
         <router-view name="header-top"></router-view>
-        <router-view></router-view>
+
+        <transition name="fade" mode="out-in" appear>
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 
@@ -14,5 +17,18 @@
 </script>
 
 <style lang="sass">
-    @import "../../node_modules/bulma/bulma";
+    @import "../../node_modules/bulma/bulma"
+</style>
+
+<style>
+    .fade-enter {
+        opacity: 0;
+    }
+    .fade-enter-active {
+        transition: opacity .3s ease-out;
+    }
+    .fade-leave-active {
+        opacity: 0;
+        transition: opacity .1s ease-out;
+    }
 </style>
