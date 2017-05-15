e8c11902dc5067ba97ed651b17ba83241f5296e5
fsarachu <fsarachu@gmail.com>
2017-05-15 16:31:14 +0200

    Add logout button

--- src/views/layouts/Header.vue
+++ src/views/layouts/Header.vue
@@ -2,11 +2,28 @@
     <nav class="nav has-shadow">
         <div class="container">
 
-            <div class="nav-center">
+            <div class="nav-left">
                 <router-link :to="{name: 'home'}" class="nav-item is-tab" exact>Home</router-link>
                 <router-link :to="{name: 'quotes'}" class="nav-item is-tab">Quotes</router-link>
+            </div>
+
+            <div v-if="isAuthenticated" class="nav-right">
+                <a @click.prevent="logout" class="nav-item is-tab">Log out</a>
             </div>
 
         </div>
     </nav>
 </template>
+
+<script>
+    import {mapActions, mapGetters} from 'vuex';
+
+    export default {
+        computed: {
+            ...mapGetters(['isAuthenticated']),
+        },
+        methods: {
+            ...mapActions(['logout']),
+        }
+    }
+</script>
