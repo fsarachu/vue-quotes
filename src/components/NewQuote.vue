0964d4053684aecf4c6936a8a1d564bd2210b8c5
fsarachu <fsarachu@gmail.com>
2017-05-15 05:39:01 +0200

    Add loading spinner when submitting quote

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -2,30 +2,37 @@
     <form @submit.prevent="onSubmit">
         <div class="field has-addons">
             <p class="control is-expanded">
-                <input v-model="quoteContent" class="input" type="text" placeholder="Enter an inspiring quote" autofocus>
+                <input v-model="quote.content" :disabled="isSubmittingQuote" class="input" type="text" placeholder="Enter an inspiring quote"
+                       autofocus>
             </p>
             <p class="control">
-                <button class="button is-primary">Submit</button>
+                <button :class="{'is-loading': isSubmittingQuote}" class="button is-primary">Submit</button>
             </p>
         </div>
     </form>
 </template>
 
 <script>
-    import {mapActions} from 'vuex';
+    import {mapActions, mapGetters} from 'vuex';
 
     export default {
         data() {
             return {
-                quoteContent: ''
-            }
+                quote: {
+                    content: '',
+                }
+            };
+        },
+        computed: {
+            ...mapGetters(['isSubmittingQuote','decodedToken']),
         },
         methods: {
-            ...mapActions(['addQuote']),
+            ...mapActions(['addQuote', 'setIsSubmittingQuote']),
             onSubmit() {
-                this.addQuote(this.quoteContent);
-                this.quoteContent = '';
-            }
+                this.setIsSubmittingQuote(true);
+                this.addQuote(this.quote);
+                this.quote.content = '';
+            },
         }
     };
 </script>
